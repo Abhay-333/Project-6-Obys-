f0cc93d26399524adda0dac91eb85df00acc351f
Abhay-333 <140523711+Abhay-333@users.noreply.github.com>
2024-03-08 18:04:44 +0530

    Page1 Almost done

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
 var incH4 = document.querySelector('.line-part1 h4')
 var counter = 0
 var time = gsap.timeline()
+var cursor = document.querySelector(".cursor")
 
-gsap.from(".line1 h1", {
-    y: 100,
-    opacity: 0,
-    stagger: 0.2,
-    delay: 0.5,
-})
+function loading(){
+    gsap.from(".line1 h1", {
+        y: 100,
+        opacity: 0,
+        stagger: 0.2,
+        delay: 0.5,
+    })
+    
+    time.from(".line1 h4", {
+        opacity: 0,
+        delay: 1,
+        onStart: function () {
+    
+            clear = setInterval(function () {
+                var clear
+                if (counter < 100) {
+                    counter++
+                    incH4.innerHTML = counter
+                }
+                else {
+                    // incH4.innerHTML = 100
+                    clearInterval(clear)
+                }
+            }, 30)
+        }
+    
+    })
+    
+    time.to(".loading-screen h1, h4",{
+        opacity:0,
+        duration:1,
+        delay:2.8,
+    })
+    
+    time.to(".loading-screen",{
+        y:-1000,
+        duration:0.8,
+        display: "none"
+    })
+    
+    time.to(".line1 span",{
+        opacity:0,
+        duration:0.1,
+    })
+    
+}
 
-time.from(".line1 h4", {
-    opacity: 0,
-    delay: 1,
-    onStart: function () {
+function mouseFollower(){
+    document.addEventListener("mousemove",function(details){
+        gsap.to(".cursor",{
+            duration:0.1,
+            top:details.clientY,
+            left:details.clientX
+        })
+    })
+}
 
-        var clear
-        clear = setInterval(function () {
-            if (counter < 100) {
-                counter++
-                incH4.innerHTML = counter
-            }
-            else {
-                incH4.innerHTML = 100
-                clearInterval(clear)
-            }
-        }, 30)
-    }
+Shery.makeMagnet(".nav-part2 h5");
 
-})
 
-time.to(".loading-screen h1, h4",{
-    opacity:0,
-    duration:1,
-    delay:2.8,
-})
-
-time.to(".loading-screen",{
-    y:-1000,
-    duration:0.8,
-    display: "none"
-})
-
-time.to(".line1 span",{
-    opacity:0,
-    duration:0.1,
-})
+mouseFollower()
+loading()
